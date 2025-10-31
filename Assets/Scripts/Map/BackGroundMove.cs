c360e1d66a348b8072c4bbe0caa2628f2aa76a12
kwang <kwanggod00@gamil.com>
2025-10-31 19:27:56 +0900

    Fixed : Maping
    맵만드는중

---
+++ Assets/Scripts/Map/BackGroundMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour
+{
+    private Renderer rend;
+    float offsetX = 0f;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        offsetX += GameManager.instance.groundSpeed*0.0001f;
+        rend.material.mainTextureOffset = new Vector2(offsetX, 0);
+    }
+}
