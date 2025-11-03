66d441d175633f1e1c130a13f922fc071761de2b
haramey <40591849+haramey123@users.noreply.github.com>
2025-11-03 19:11:56 +0900

    Fix: 인게임 UI 버튼 오작동 수정

---
+++ Assets/Scripts/UI/IPointerHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class IPointerHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public UnityEvent onPointerDown; // 누를 때 실행
+    public UnityEvent onPointerUp; // 뗄 때 실행
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        onPointerDown?.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        onPointerUp?.Invoke();
+    }
+}
